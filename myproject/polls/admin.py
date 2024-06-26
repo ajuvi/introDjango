08d39ff7431862c94345f00c1f67adf77b22ad8c
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-06-26 08:05:53 +0200

    artur

--- myproject/polls/admin.py
+++ myproject/polls/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Vote
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -15,3 +15,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Vote)
--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -2,8 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -21,3 +19,14 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Vote(models.Model):
+    nom_usuari = models.CharField(max_length=200)
+    valoracio = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.nom_usuari} - {self.question} -  {self.choice}'
+
+
